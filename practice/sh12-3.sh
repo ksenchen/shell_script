89768dc34fbce742a078cdb7469a077dcd0e3bfc
ksenchen <ksenchen@163.com>
2017-02-14 16:32:07 +0800

    modify some practice

--- practice/sh12-3.sh
+++ practice/sh12-3.sh
@@ -8,21 +8,23 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 function printit(){
-	echo -n "Your choice is $1\n"
+	echo -n "Your choice is $1"
 }
 echo "This program will print your selection!"
+choice=`echo $1|tr 'a-z' 'A-z'`
 
 case $1 in
 "one" )
-	printit ONE
+	printit $choice
 	;;
 "two" )
-	printit TWO
+	printit $choice
 	;;
 "three")
-	printit THREE
+	printit $choice
 	;;
 * )
 	echo "Usage $0 {one|two|three}"
 	;;
 esac
+echo
